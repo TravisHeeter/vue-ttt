bc62735bb6d4c7d67cd42beb3ee21df955a2252b
TravisHeeter <heetertc@gmail.com>
2020-10-16 12:07:16 -0400

    Added TicTacToe and Cell files

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
-	<div id="app">
-		<img alt="Vue logo" src="https://vuejs.org/images/logo.png">
-		<HelloWorld msg="Welcome to Your Vue.js App" />
-	</div>
+  <div id="app">
+    <TicTacToe />
+  </div>
 </template>
 
 <script>
-	import HelloWorld from './components/HelloWorld.vue'
+import TicTacToe from "./TicTacToe.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    TicTacToe
   }
-}
+};
 </script>
 
 <style>
-	#app {
-		font-family: Avenir, Helvetica, Arial, sans-serif;
-		-webkit-font-smoothing: antialiased;
-		-moz-osx-font-smoothing: grayscale;
-		text-align: center;
-		color: #2c3e50;
-		margin-top: 60px;
-	}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
