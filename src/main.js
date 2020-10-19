7304fc7566710dd61810b60dffcb8d401097c6c4
TravisHeeter <heetertc@gmail.com>
2020-10-19 08:25:00 -0400

    1 file added, 1 deleted

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from "vue";
-import Board from "./components/Board";
-import Square from "./components/Square";
-
-const app = createApp({
-  /* options */
-});
-
-app.component("tic-tac-toe", Board);
