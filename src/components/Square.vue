6bc6621f424449618a39d5b00b6e52128ce762bb
TravisHeeter <heetertc@gmail.com>
2020-10-19 19:33:21 -0400

    3 files modified

--- src/components/Square.vue
+++ src/components/Square.vue
@@ -1,81 +1,66 @@
-import { ref } from 'vue'
-
-setup() {
-const board = ref(Array(9).fill(null));
-const playerValue = ref('X');
-const markSquare = (i) => {
-const boardCopy = board.value.slice();
-boardCopy[i] = playerValue.value;
-board.value = boardCopy;
-playerValue.value === 'X' ? (playerValue.value = 'O') : (playerValue.value = 'X');
-}
-return {
-board,
-playerValue,
-markSquare,
-}
-}
-
 <template>
-	<button class="square" :name="label" :disabled="winner || value">
+  <button class="square" :name="label" :disabled="winner || value">
     {{ value }}
   </button>
 </template>
 
 <script>
-	export default {
-    props: {
-      label: String,
-      value: {
-        type: String,
-        default: " ",
-      },
-      winner: null,
+import { defineComponent, PropType, toRefs } from "vue";
+export default defineComponent({
+  props: {
+    label: {
+      type: String
     },
-  };
+    value: {
+      type: String,
+      default: " "
+    },
+    winner: { type: String }
+  }
+});
 </script>
 
 <style scoped>
-	.square {
-		border: none;
-		width: 10rem;
-		height: 10rem;
-		background: none;
-		color: inherit;
-		font-size: 3rem;
-		font-weight: 700;
-	}
+.square {
+  border: none;
+  width: 10rem;
+  height: 10rem;
+  background: none;
+  color: inherit;
+  font-size: 3rem;
+  font-weight: 700;
+}
 
-	.square:hover {
-		cursor: pointer;
-	}
+.square:hover {
+  cursor: pointer;
+}
 
-	.square:focus {
-		outline: none;
-		background: #41b88330;
-	}
+.square:focus {
+  outline: none;
+  background: #41b88330;
+}
 
-	.square:first-child,
-	.square:nth-child(2),
-	.square:nth-child(3) {
-		border-top: none;
-	}
+.square:first-child,
+.square:nth-child(2),
+.square:nth-child(3) {
+  border-top: none;
+}
 
-	.square:nth-child(3),
-	.square:nth-child(6),
-	.square:last-child {
-		border-right: none;
-	}
+.square:nth-child(3),
+.square:nth-child(6),
+.square:last-child {
+  border-right: none;
+}
 
-	.square:nth-child(7),
-	.square:nth-child(8),
-	.square:last-child {
-		border-bottom: none;
-	}
+.square:nth-child(7),
+.square:nth-child(8),
+.square:last-child {
+  border-bottom: none;
+}
 
-	.square:first-child,
-	.square:nth-child(4),
-	.square:nth-child(7) {
-		border-left: none;
-	}
+.square:first-child,
+.square:nth-child(4),
+.square:nth-child(7) {
+  border-left: none;
+}
 </style>
