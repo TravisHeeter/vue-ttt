a0066fe10337ac11aa2f28de05964ef4dd1c7c9f
TravisHeeter <heetertc@gmail.com>
2020-10-19 08:03:22 -0400

    1 file modified

--- src/components/Square.vue
+++ src/components/Square.vue
@@ -1,23 +1,63 @@
 <template>
-  <div class="cell">
-    <span v-if="value !== ''">{{ value }}</span>
-  </div>
+	<button class="square" :name="label" :disabled="winner || value">
+    {{ value }}
+  </button>
 </template>
+
 <script>
-export default {
-  props: {
-    value: String
-  }
-};
+	export default {
+    props: {
+      label: String,
+      value: {
+        type: String,
+        default: " ",
+      },
+      winner: null,
+    },
+  };
 </script>
-<style>
-.cell {
-  width: 64px;
-  height: 64px;
-  border: 2px solid rgb(75, 75, 75);
-  font-size: 42px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
+
+<style scoped>
+	.square {
+		border: none;
+		width: 10rem;
+		height: 10rem;
+		background: none;
+		color: inherit;
+		font-size: 3rem;
+		font-weight: 700;
+	}
+
+	.square:hover {
+		cursor: pointer;
+	}
+
+	.square:focus {
+		outline: none;
+		background: #41b88330;
+	}
+
+	.square:first-child,
+	.square:nth-child(2),
+	.square:nth-child(3) {
+		border-top: none;
+	}
+
+	.square:nth-child(3),
+	.square:nth-child(6),
+	.square:last-child {
+		border-right: none;
+	}
+
+	.square:nth-child(7),
+	.square:nth-child(8),
+	.square:last-child {
+		border-bottom: none;
+	}
+
+	.square:first-child,
+	.square:nth-child(4),
+	.square:nth-child(7) {
+		border-left: none;
+	}
 </style>
